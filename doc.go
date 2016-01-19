1935b0211e883b07cc994014829c5fb216cc3b2a
jpeterson <jpeterson@gettyimages.com>
2016-01-18 18:56:18 -0600

    docs: add godoc

---
+++ doc.go
@@ -0,0 +1,12 @@
+/*
+Package sleepwalker is a simple REST client.
+
+It was extracted from an SDK the author built while learning Go. No claim is
+made as to the quality or utility of this code. It's safe to assume there are
+more fully featured and better-written Go REST clients available.
+
+The code is in a pre-release state and is subject to (drastically) change as
+its interface between the SDK client and other related packages is discovered
+and refined.
+*/
+package sleepwalker
